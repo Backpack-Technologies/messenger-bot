a62c37e955ae7767af16ddd2e3b9dcb9cfa5933b
Shubhashis Roy <iamdipta@gmail.com>
2019-01-22 13:49:09 +0600

    amazon different type of link handled

--- Components/AmazonPrice.js
+++ Components/AmazonPrice.js
@@ -2,7 +2,11 @@ const rp = require('request-promise');
 
 const getAsinFromLink = (link) => {
     let pos = link.search('/dp/');
-    pos += 4;
+    if(pos == -1) {
+        pos = link.search('/product/');
+        pos += 9;
+    }
+    else pos += 4;
     let asin = "";
     while(pos < link.length && asin.length < 10) asin += link[pos], pos++;
     return asin;
